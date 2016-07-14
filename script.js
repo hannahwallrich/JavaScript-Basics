a97652f6cd42841d673d42c815fa5bff21b5f0a6
Hannah Wallrich <hannahwallrich@gmail.com>
2016-07-14 16:23:06 -0400

    made some fruity variables

---
+++ script.js
@@ -0,0 +1,21 @@
+//VARIABLES
+//make some variables
+var berries = ["strawberry", "blueberry", "raspberry", "blackberry"]
+var other_fruit = ["apple", "kiwi"]
+//make an array(s)
+var fruit_types = ["berries", "other_fruit"]
+//create arrayception (multidimensional array)
+//var fave_fruit = prompt("What is your favorite fruit?")
+
+//DEBUGGING
+$(document).ready(function(){
+    alert('Get ready for FRUITS');
+    //pop up window linking in a variable
+    confirm("You like fruit, right?");
+	//ask question with boulean answer (true/false)
+	var fave_fruit = prompt("What is your favorite fruit?")
+	console.log(fave_fruit);
+	//ask open question and print answer to console
+  });
+
+//var fave_fruit = prompt("What is your favorite fruit?")
